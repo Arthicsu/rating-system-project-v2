6b1842ff68e6163dae93f43f4abf44ef156c45ea
Arthicsu <ronivl@yandex.ru>
2026-02-18 18:23:33 +0300

    - [WIP] Change Teacher model to Staff model - deleted hardcode list[tuple] in Faculty model - added Department model - changed Group model

--- app/backend/university_structure/models.py
+++ app/backend/university_structure/models.py
@@ -2,28 +2,32 @@ from django.db import models
 from django.conf import settings
 
 class Faculty(models.Model):
-    FACULTY_CHOICES = [
-        ('EiEB', 'Экономика и экономическая безопасность'),
-        ('IT', 'Информационные технологии'),
-    ]
-    
     name = models.CharField("Название факультета", max_length=255, unique=True)
     short_name = models.CharField("Сокращение", max_length=20, unique=True)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return self.short_name
 
     class Meta:
         verbose_name = "Факультет"
         verbose_name_plural = "Факультеты"
 
+class Department(models.Model):
+    name = models.CharField("Название кафедры", max_length=255, unique=True)
+    short_name = models.CharField("Сокращение", max_length=20, unique=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='departments', null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.short_name
+
+    class Meta:
+        verbose_name= "Кафедра"
+        verbose_name_plural = "Кафедры"
+
 class Group(models.Model):    
     name = models.CharField(max_length=50, unique=True)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='groups', null=True, blank=True)
-    course = models.PositiveSmallIntegerField()
-    curator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, 
-        null=True, blank=True,
-        related_name='curated_groups')
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='groups', null=True, blank=True)
+    course = models.PositiveSmallIntegerField("Курс")
     
     def __str__(self):
         return self.name
@@ -32,18 +36,18 @@ class Group(models.Model):
         verbose_name = "Группа"
         verbose_name_plural = "Группы"
 
-class Teacher(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        null=True, blank=True,
-        related_name='teacher_profile'
-    )
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='teachers', null=True, blank=True)
-    phone = models.CharField("Телефон", max_length=20)
-    position = models.CharField("Должность", max_length=100, blank=True)
+class Staff(models.Model):
+    """
+    Модель сотрудника университета. Позже распишу что и как (мне пока лень)
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='staff_profile')
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='staff', null=True, blank=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='staff', null=True, blank=True)
 
     def __str__(self):
-        return f"Преподаватель: {self.user.get_full_username()}"
+        return f"{self.user.get_full_username()}"
 
     class Meta:
-        verbose_name = "Профиль преподавателя"
-        verbose_name_plural = "Профили преподавателей"
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
