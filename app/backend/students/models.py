f6f3e61e876c3d372c4573835e07b31d3309526a
Arthicsu <ronivl@yandex.ru>
2026-03-12 00:44:48 +0300

    - [WIP] Added new models and updated old models in Students - Updated serializers.py in Students - Fixes - Removed Herobrine

--- app/backend/students/models.py
+++ app/backend/students/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from university_structure.models import Group, Faculty, Department
-from .scoring import calculate_achievement_score, get_choices_from_config
+
 
 class Student(models.Model):
     """
@@ -61,6 +61,105 @@ class Student(models.Model):
         group_name = self.group.name if self.group else "Без группы"
         return f"{self.full_name} ({group_name})"
 
+class MetadataBase(models.Model):
+    """
+    Абстрактная базовая модель для всех справочников (метаданные)
+    """
+    code = models.CharField("Код", max_length=50, unique=True)
+    label = models.CharField("Название", max_length=255)
+
+    class Meta:
+        abstract = True
+        ordering = ['label']
+
+    def __str__(self):
+        return self.label
+
+class Level(MetadataBase):
+    class Meta(MetadataBase.Meta):
+        verbose_name = "Уровень достижения"
+        verbose_name_plural = "Уровни достижений"
+
+class AchievementResult(MetadataBase):
+    class Meta(MetadataBase.Meta):
+        verbose_name = "Результат достижения"
+        verbose_name_plural = "Результаты достижений"
+
+class DocType(MetadataBase):
+    class Meta(MetadataBase.Meta):
+        verbose_name = "Тип документа"
+        verbose_name_plural = "Типы документов"
+
+class DocumentStatus(MetadataBase):
+    class Meta(MetadataBase.Meta):
+        verbose_name = "Статус документа"
+        verbose_name_plural = "Статусы документов"
+
+class Category(models.Model):
+    """
+    Категория достижения (например: academic, sport, science)
+    """
+    code = models.CharField(max_length=50, unique=True, verbose_name="Код")
+    label = models.CharField(max_length=255, verbose_name="Название")
+
+    class Meta:
+        """
+        Метакласс для настройки поведения модели.
+
+        Задаёт человекочитаемые названия и порядок сортировки записей.
+        """
+        verbose_name = "Категория достижения"
+        verbose_name_plural = "Категории достижений"
+
+    def __str__(self):
+        return self.label
+
+class AchievementType(models.Model):
+    """
+    Подвид достижения (например: olympiad, conference, publication)
+    
+    флаги для клиента needs_level и needs_result: нужно ли запрашивать уровень и результат
+    """
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_types')
+    code = models.CharField(max_length=50, verbose_name="Код")
+    label = models.CharField(max_length=100, verbose_name="Название")
+    
+
+    needs_level = models.BooleanField(default=False, verbose_name="Требует указания уровня")
+    needs_result = models.BooleanField(default=False, verbose_name="Требует указания результата")
+
+    class Meta:
+        unique_together = ('category', 'code')
+        verbose_name = "Тип достижения"
+        verbose_name_plural = "Типы достижений"
+
+    def __str__(self):
+        return f"{self.category.label} - {self.label}"
+
+class ScoringRule(models.Model):
+    """
+    Правила начисления баллов
+    Пока добавить нечего
+    """
+    
+    achievement_type = models.ForeignKey(AchievementType, on_delete=models.CASCADE, related_name='rules')
+    
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Уровень") 
+    result = models.ForeignKey(AchievementResult, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Результат")
+    
+    score = models.IntegerField(verbose_name="Количество баллов")
+
+    def __str__(self):
+        return f"{self.achievement_type} | Уровень: {self.level or 'Любой'} | Результат: {self.result or 'Любой'} -> {self.score}"
+
+    class Meta:
+        """
+        Метакласс для настройки поведения модели.
+
+        Задаёт человекочитаемые названия и порядок сортировки записей.
+        """
+        verbose_name = "Правило счёта"
+        verbose_name_plural = "Правила счёта"
 
 class Document(models.Model):
     """
@@ -70,29 +169,27 @@ class Document(models.Model):
     учебных, культурно-творческих, общественных, спортивных и научно-исследовательских.
     
     Каждый документ привязан к студенту, содержит метаданные (категория, уровень, результат),
-    ссылку на файл и проходит процесс модерации (на рассмотрении, подтверждён, отклонён).
-
-    Примечание: использует динамически загружаемые choices через JSON-конфигурацию.
-    
+    ссылку на файл и проходит процесс модерации (на рассмотрении, подтверждён, отклонён).   
     """
+
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='student_documents')
-    
+    # пока заглушка date_received
     date_received = models.DateField("Дата получения", default=timezone.now) 
     verified_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='verified_documents', verbose_name='Кем проверено')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     
-    category = models.CharField(max_length=50, choices=get_choices_from_config('categories'))
-    sub_type = models.CharField("Подтип", max_length=50, choices=get_choices_from_config('sub_types'), default='other')
-    level = models.CharField("Уровень", max_length=50, choices=get_choices_from_config('metadata.levels'), default='none')
-    result = models.CharField("Результат", max_length=50, choices=get_choices_from_config('metadata.results'), default='other')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Категория")
+    sub_type = models.ForeignKey(AchievementType, on_delete=models.PROTECT, verbose_name="Подтип")
+    level = models.ForeignKey(Level, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Уровень")
+    result = models.ForeignKey(AchievementResult, on_delete=models.PROTECT, null=True, blank=True, verbose_name="Результат")
     achievement = models.CharField("Название достижения", max_length=255)
-    doc_type = models.CharField("Тип документа", max_length=50, choices=get_choices_from_config('metadata.doc_types'), default='other')
+    doc_type = models.ForeignKey(DocType, on_delete=models.PROTECT, verbose_name="Тип документа")
     
     original_file_name = models.CharField(max_length=255, default='NO_FILENAME')
     file_url = models.URLField(max_length=500, null=True, blank=True)
 
     score = models.PositiveIntegerField("Баллы", default=0)
-    status = models.CharField(max_length=20, choices=get_choices_from_config('metadata.statuses'), default='pending')
+    status = models.ForeignKey(DocumentStatus, on_delete=models.PROTECT, verbose_name="Статус")
     rejection_reason = models.TextField("Причина отказа", blank=True, null=True)
     
     def __str__(self) -> str:
@@ -106,7 +203,7 @@ class Document(models.Model):
         Возвращает:
             str: Строка с названием достижения и статусом.
         """
-        return f"{self.achievement} ({self.get_status_display()})"
+        return f"{self.achievement} ({self.status})"
     
     def save(self, *args, **kwargs):
         """
@@ -120,8 +217,12 @@ class Document(models.Model):
             *args: Позиционные аргументы, передаваемые в родительский метод.
             **kwargs: Именованные аргументы, передаваемые в родительский метод.
         """
+        from .scoring import calculate_achievement_score
         self.score = calculate_achievement_score(
-            self.category, self.sub_type, self.level, self.result
+            self.category.code, 
+            self.sub_type.code, 
+            self.level.code if self.level else None, 
+            self.result.code if self.result else None
         )
         super().save(*args, **kwargs)
 
--- app/backend/students/serializers.py
+++ app/backend/students/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from university_structure.models import Faculty
-from students.models import Student
-
-from .models import Document
+from .models import Student, Document, Category
 
 class StudentRatingSerializer(serializers.ModelSerializer):
     """
@@ -36,11 +34,12 @@ class StudentRatingSerializer(serializers.ModelSerializer):
         ]
 
 class DocumentSerializer(serializers.ModelSerializer):
-    category_display = serializers.CharField(source='get_category_display', read_only=True)
-    sub_type_display = serializers.CharField(source='get_sub_type_display', read_only=True)
-    level_display = serializers.CharField(source='get_level_display', read_only=True)
-    result_display = serializers.CharField(source='get_result_display', read_only=True)
-    doc_type_display = serializers.CharField(source='get_doc_type_display', read_only=True)
+    category_display = serializers.CharField(source='category.label', read_only=True)
+    sub_type_display = serializers.CharField(source='sub_type.label', read_only=True)
+    level_display = serializers.CharField(source='level.label', read_only=True, default=None)
+    result_display = serializers.CharField(source='result.label', read_only=True, default=None)
+    doc_type_display = serializers.CharField(source='doc_type.label', read_only=True)
+    status_display = serializers.CharField(source='status.code', read_only=True)
 
     class Meta:
         model = Document
@@ -53,7 +52,7 @@ class DocumentSerializer(serializers.ModelSerializer):
             'achievement',
             'rejection_reason', 
             'score', 
-            'status',
+            'status', 'status_display',
             'doc_type', 'doc_type_display', 
             'file_url', 
             'original_file_name', 
@@ -63,15 +62,15 @@ class DocumentSerializer(serializers.ModelSerializer):
 class PendingDocumentSerializer(DocumentSerializer):
     student_id = serializers.IntegerField(source='student.id', read_only=True)
     student_name = serializers.CharField(source='student.user.get_full_username', read_only=True)
-    group_id = serializers.CharField(source='student.group.id', read_only=True, default="—")
-    record_book = serializers.CharField(source='student.record_book', read_only=True)
+    group_id = serializers.IntegerField(source='student.group.id', read_only=True)
+    record_book = serializers.CharField(source='student.record_book', read_only=True, default="—")
 
     class Meta(DocumentSerializer.Meta):
         fields = DocumentSerializer.Meta.fields + ['student_id', 'student_name', 'group_id', 'record_book']
 
 class StudentProfileSerializer(serializers.ModelSerializer):
     group = serializers.CharField(source='group.name', read_only=True, default="Без группы")
-    group_id = serializers.CharField(source='group.id', read_only=True)
+    group_id = serializers.IntegerField(source='group.id', read_only=True)
     course = serializers.IntegerField(source='group.course', read_only=True)
     faculty = serializers.CharField(source='faculty.short_name', read_only=True, default="—")
     documents = DocumentSerializer(many=True, read_only=True, source='student_documents')
@@ -85,4 +84,11 @@ class StudentProfileSerializer(serializers.ModelSerializer):
             'group', 'group_id', 'course', 'faculty',
             'academic_score', 'research_score', 'sport_score', 'social_score', 'cultural_score', 'total_score',
             'documents',
+        ]
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            'code', 'label',
         ]
