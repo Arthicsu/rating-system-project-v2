bf1d94ace24cf0c83dfea0b47d5223200eca1a3c
Arthicsu <ronivl@yandex.ru>
2026-03-31 21:58:12 +0300

    - some fixes - new migration file's

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -207,6 +207,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
