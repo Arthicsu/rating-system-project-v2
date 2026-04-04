13369f13247dd7d08e4ce406efa2c6c1c396abf8
Arthicsu <ronivl@yandex.ru>
2026-04-04 20:05:11 +0300

    - Updated docker-compose.yml - Added new entrypoint for SeaweedFS - Updated entrypoint.sh for backend - Updated .env.example - Removed Herobrine

--- app/backend/entrypoint.sh
+++ app/backend/entrypoint.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
-# Функция для проверки доступности SeaweedFS
-wait_for_seaweed() {
-  echo "Waiting for SeaweedFS S3 API (seaweedfs:8333)..."
-  until python -c "import socket; s = socket.socket(); s.connect(('seaweedfs', 8333))" 2>/dev/null; do
-    sleep 1
-  done
-  echo "SeaweedFS is up and running!"
+check_vars() {
+  if [ -z "$SEAWEEDFS_ACCESS_KEY" ] || [ -z "$SEAWEEDFS_SECRET_KEY" ]; then
+    echo "ERR: SEAWEEDFS_ACCESS_KEY and SEAWEEDFS_SECRET_KEY must be set!"
+    exit 1
+  fi
 }
 
-wait_for_seaweed
-
-echo "Ensuring S3 bucket '${SEAWEEDFS_BUCKET_NAME}' exists..."
-python << END
+create_bucket() {
+  echo "Ensuring S3 bucket '${SEAWEEDFS_BUCKET_NAME}' exists..."
+  python << END
 import boto3
 import os
 
 s3 = boto3.resource('s3',
-    endpoint_url=os.getenv('SEAWEEDFS_ENDPOINT_URL', 'http://seaweedfs:8333'),
-    aws_access_key_id=os.getenv('SEAWEEDFS_ACCESS_KEY', 'some_access_key'),
-    aws_secret_access_key=os.getenv('SEAWEEDFS_SECRET_KEY', 'some_secret_key'),
-    region_name='us-east-1'
+    endpoint_url=os.getenv('SEAWEEDFS_ENDPOINT_URL'),
+    aws_access_key_id=os.getenv('SEAWEEDFS_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('SEAWEEDFS_SECRET_KEY'),
+    region_name='local'
 )
 bucket_name = os.getenv('SEAWEEDFS_BUCKET_NAME', 'achievement')
 bucket = s3.Bucket(bucket_name)
@@ -28,15 +25,20 @@ bucket = s3.Bucket(bucket_name)
 if bucket.creation_date is None:
     print(f"Bucket '{bucket_name}' not found. Creating...")
     bucket.create()
-    # Устанавливаем публичный доступ (policy), если это нужно для раздачи файлов напрямую
     print(f"Bucket '{bucket_name}' created successfully.")
 else:
     print(f"Bucket '{bucket_name}' already exists.")
 END
+}
+
+check_vars
+create_bucket
+
 
 echo "Running migrations..."
 python manage.py migrate
 
+# Для Nginx
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
---
+++ seaweedfs-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+mkdir -p /etc/seaweedfs
+# Генерация s3.json
+echo '{
+  "buckets": [{"name": "'"${AWS_STORAGE_BUCKET_NAME}"'"}],
+  "identities": [{
+    "name": "admin",
+    "credentials": [{
+      "accessKey": "'"${AWS_ACCESS_KEY_ID}"'",
+      "secretKey": "'"${AWS_SECRET_ACCESS_KEY}"'"
+    }],
+    "actions": ["Admin:.*", "Read:.*", "Write:.*", "List:.*"]
+  }]
+}' > /etc/seaweedfs/s3.json
+
+exec /usr/bin/weed server \
+  -dir=/data \
+  -s3 \
+  -s3.config=/etc/seaweedfs/s3.json \
+  -master.volumeSizeLimitMB=1024 \
+  -filer.defaultReplicaPlacement=000
